422b033232647acc5a17456875172ceddbea6299
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-12-03 15:54:58 +0900

    3

--- main.c
+++ main.c
@@ -9,24 +9,30 @@ struct Book{
 }; 
 
 int main(int argc, char *argv[]){
-	struct Book *p;     //struct Book형 (int형 처럼) 포인터 p를 생성. 
+	int i;
+	struct Book **bookshelf;     //struct Book형 (int형 처럼) 포인터 p를 생성. 
+
+	bookshelf =  (struct Book**)malloc(3*sizeof(struct Book*));//3개의 Book 구조체 변수 동적 할당  
+ 	for(i=0;i<3;i++){
+		bookshelf[i]=(struct Book*)malloc(10*sizeof(struct Book));
+	}
 	
-	p =(struct Book*)malloc(2*sizeof(struct Book));
+	bookshelf[1][3].number=5;
+	strcpy(bookshelf[1][3].title,"C programming");
 	
-	if(p ==NULL){
-		printf("메모리 할당 오류\n");
-		return -1;
-	}	
+	(bookshelf[2]+4)->number =3;
+	strcpy((bookshelf[2]+4)->title,"Communications Theory");
 	
-	p->number=1;
-	strcpy(p->title, "C programming");
+	printf("book(1,3): %i, %s\n",(bookshelf[1]+3)->number,(bookshelf[1]+3)->title);
+	printf("book(2,4): %i, %s\n",bookshelf[2][4].number,bookshelf[2][4].title);
 	
-	(p+1)->number =2;
-	strcpy((p+1)->title, "Electronics");
-	printf(" %s %s \n",p->title, (p+1)->title);
+	for(i=0;i<3;i++)
+		free(bookshelf[i]);
+	free(bookshelf);
 	
-	free(p);
-	return;
+	return ;
+
+	
 }
 
 
